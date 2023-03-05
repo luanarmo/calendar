506edc2b26471a5ee9cda9e91d9698a02d0717e7
luanarmo <ang3l_morales800@live.com>
2023-03-05 16:27:46 -0600

    Updates

--- assets/secreto-047680c0.js
+++
@@ -1,1 +0,0 @@
-import{T as t}from"./style-cf5b8e01.js";document.addEventListener("DOMContentLoaded",()=>{t({text:"Secretiño",position:"center",style:{background:"linear-gradient(to right, #00b09b, #96c93d)"}}).showToast()});
